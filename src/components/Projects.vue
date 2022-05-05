1163c7465a81e488dfaf2c91974249b2120e7550
n.assel <nils.assel@flyeralarm.com>
2022-05-05 08:56:13 +0200

    Add project to Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,14 +1,26 @@
 <template>
-    <div class="projects-div">
-        <a href="https://github.com/nils-developer/nils-developer" target="_blank">
+    <div class="projects">
+        <div class="projects-div">
+            <a href="https://github.com/nils-developer/nils-developer" target="_blank">
 
-            <div class="projects-div-content">
-                <h3>Github Profile</h3>
-                <p class="projects-p">Das ist mein Github Profil. In diesem Profil findest du Projekte an denen ich gerade arbeite und 
-                    Projekte die ich bereits abgeschlossen habe.
-                </p>
-            </div>
-        </a>
+                <div class="projects-div-content">
+                    <h3>Github Profile</h3>
+                    <p class="projects-p">Das ist mein Github Profil. In diesem Profil findest du Projekte an denen ich gerade arbeite und 
+                        Projekte die ich bereits abgeschlossen habe.
+                    </p>
+                </div>
+            </a>
+        </div>
+        <div class="projects-div">
+            <a href="https://github.com/nils-developer/awake-vue-frontend" target="_blank">
+                <div class="projects-div-content">
+                    <h3>Project: Awake</h3>
+                    <p class="projects-p">
+                        Ein simples Frontend Projekt mit Vue.js
+                    </p>
+                </div>
+            </a>
+        </div>
     </div>
 </template>
 
@@ -19,14 +31,21 @@ export default {
 </script>
 
 <style scoped>
+.projects {
+    display: flex; 
+    flex-direction: column;
+
+    max-width: 45%;
+}
+
 .projects-div {
     display: flex;
-    justify-content: center;
     
     align-items: center;
 
     text-align: left;
-    max-width: 45%;
+
+    margin-bottom: 1.5rem;
 
     height: 20vh;
     min-height: 10rem;
